bdbe1c3220d419f387286a92bd5d93d8119ec068
Agbisit <31266732+Agbisit@users.noreply.github.com>
2017-12-05 07:01:54 +0800

    Update process.js

--- reactMinimal/www/js/process.js
+++ reactMinimal/www/js/process.js
@@ -1,8 +1,5 @@
 let process = {
 	menu:function(){
-		<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		let content = <ul><center>
 		<h1>Recipe List</h1>
 			<a href='#' onClick={process.page1}><button class="accordion">Spicy Chilli Chicken Stir-Fry</button></a><br/>
